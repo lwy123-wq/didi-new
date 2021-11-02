731e55cf68248c4b65095d64f16dd75b3dbd1e81
lwy123-wq <l2158223895@qq.com>
2021-11-02 19:46:20 +0800

    address

--- src/main/java/sun/com/didi/controller/AddressController.java
+++ src/main/java/sun/com/didi/controller/AddressController.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.com.didi.interceptor.AddressedInterceptor;
 import sun.com.didi.service.AddressServiceImpl;
 
 @Controller
 public class AddressController {
     @Autowired
     private AddressServiceImpl addressService;
+    @Autowired
+    private AddressedInterceptor addressedInterceptor;
     @PostMapping(value = "/address")
     @ResponseBody
-    public String CityByIp(String address){
-        return addressService.Address(address);
+    public String CityByIp(){
+        System.out.println("address"+addressedInterceptor.address);
+        return addressService.Address(addressedInterceptor.address);
+
     }
 }
--- src/main/java/sun/com/didi/interceptor/AddressedInterceptor.java
+++ src/main/java/sun/com/didi/interceptor/AddressedInterceptor.java
@@ -20,8 +20,9 @@ import java.util.Map;
 
 @Configuration
 public class AddressedInterceptor implements HandlerInterceptor {
+    public String address;
     @Autowired
-    private AddressController addressService;
+    private AddressServiceImpl addressService;
     public static String sendGet(String ip) {
         String result = "";
         BufferedReader in = null;
@@ -76,12 +77,12 @@ public class AddressedInterceptor implements HandlerInterceptor {
             String province = (String) addressInfo.get("province");
             //  String district = (String) addressInfo.get("district");
             String city = (String) addressInfo.get("city");
-            String address = nation + "-" + province + "-" + city;
-            addressService.CityByIp(address);
+            address = nation + "-" + province + "-" + city;
+            //addressService.Address(address);
         }else if (message.equals("局域网IP无法定位")){
-            String addr="天津\n";
-            addressService.CityByIp(addr);
-            System.out.println("办公区域/局域网区域"+addr);
+            address="天津\n";
+            //addressService.Address(address);
+            //System.out.println("办公区域/局域网区域"+addr);
         }
 
         return true;
