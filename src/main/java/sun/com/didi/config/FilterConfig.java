d97364736b3c9e0c835a00ca279646d8698c6d87
lwy123-wq <l2158223895@qq.com>
2021-11-16 17:11:25 +0800

    filter

---
+++ src/main/java/sun/com/didi/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package sun.com.didi.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import sun.com.didi.filter.MyFilter;
+
+@Configurable
+public class FilterConfig {
+    /**
+     * 配置跨域访问的过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean registerFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.addUrlPatterns("/*");
+        bean.setFilter(new MyFilter());
+        return bean;
+    }
+}
