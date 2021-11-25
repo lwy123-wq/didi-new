a064e9d74aeaa60859a7d73799fa070c919f16ed
lxj <1873579697@qq.com>
2021-11-25 18:48:41 +0800

    ok

--- src/main/java/sun/com/didi/controller/QueryController.java
+++ src/main/java/sun/com/didi/controller/QueryController.java
@@ -22,22 +22,51 @@ import java.util.ArrayList;
 public class QueryController {
     @Autowired
     private RecruitServiceImpl recruitService;
-    @RequestMapping(value = "/Query",method = RequestMethod.GET)
+    @RequestMapping(value = "/Lucene",method = RequestMethod.GET)
     public String Lucene(){return "/Lucene";}
     @PostMapping(value = "/query")
     @ResponseBody
-    public ArrayList<String> doc(String str) throws IOException {
+    public ArrayList<String> query(String str) throws IOException {
         Recruit byCompany = recruitService.findByCompany(str);
         Recruit byCategory = recruitService.findByCategory(str);
         ArrayList<String> list = null;
         if (str.indexOf(byCompany.getRec_company())!=-1){
-             list= QueryCompany(str);
+             list= QueryCompany(str,"Rec_company");
         }else if (str.indexOf(byCategory.getRec_category())!=-1) {
-            list=QueryCategory(str);
+            list=QueryCompany(str,"Rec_category");
         }
         return list;
     }
-    public ArrayList<String> QueryCompany(String index) throws IOException {
+    public ArrayList<String> QueryCompany(String index,String num) throws IOException {
+        ArrayList<String> list=new ArrayList<>();
+        //lucene搜索引擎
+        Directory directory = FSDirectory.open(new File("src/index").toPath());
+        IndexReader indexReader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+        //创建查询对象
+        Query query = new TermQuery(new Term(num, index));
+        //执行查询
+        TopDocs topDocs = indexSearcher.search(query, 5);
+        //共查询到的document个数
+        System.out.println("查询结果总数量：" + topDocs.totalHits);
+        //遍历查询结果
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            Document document = indexSearcher.doc(scoreDoc.doc);
+            list.add(document.get("Rec_company"));
+            System.out.println(document.get("Rec_company"));
+            System.out.println(document.get("Rec_logo"));
+            System.out.println(document.get("Rec_category"));
+            System.out.println(document.get("Rec_salary"));
+            System.out.println(document.get("Rec_Duration"));
+            System.out.println(document.get("Rec_experience"));
+
+
+        }
+        //关闭indexreader
+        indexSearcher.getIndexReader().close();
+        return list;
+    }
+/*    public ArrayList<String> QueryCategory(String index) throws IOException {
         ArrayList<String> list=new ArrayList<>();
         //lucene搜索引擎
         Directory directory = FSDirectory.open(new File("src/index").toPath());
@@ -60,39 +89,10 @@ public class QueryController {
             System.out.println(document.get("Rec_Duration"));
             System.out.println(document.get("Rec_experience"));
 
-
         }
         //关闭indexreader
         indexSearcher.getIndexReader().close();
         return list;
-    }
-    public ArrayList<String> QueryCategory(String index) throws IOException {
-        ArrayList<String> list=new ArrayList<>();
-        //lucene搜索引擎
-        Directory directory = FSDirectory.open(new File("src/index").toPath());
-        IndexReader indexReader = DirectoryReader.open(directory);
-        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
-        //创建查询对象
-        Query query = new TermQuery(new Term("Rec_company", index));
-        //执行查询
-        TopDocs topDocs = indexSearcher.search(query, 5);
-        //共查询到的document个数
-        System.out.println("查询结果总数量：" + topDocs.totalHits);
-        //遍历查询结果
-        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-            Document document = indexSearcher.doc(scoreDoc.doc);
-            list.add(document.get("Rec_company"));
-            System.out.println(document.get("Rec_company"));
-            System.out.println(document.get("Rec_logo"));
-            System.out.println(document.get("Rec_category"));
-            System.out.println(document.get("Rec_salary"));
-            System.out.println(document.get("Rec_Duration"));
-            System.out.println(document.get("Rec_experience"));
-
-        }
-        //关闭indexreader
-        indexSearcher.getIndexReader().close();
-        return list;
-    }
+    }*/
 
 }
