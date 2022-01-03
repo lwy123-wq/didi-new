0919fbba6ca834cb8a3660467a07dfe5f96742f5
lxj <1873579697@qq.com>
2022-01-03 23:18:27 +0800

    ok

--- src/main/java/sun/com/didi/controller/EsQueryController.java
+++ src/main/java/sun/com/didi/controller/EsQueryController.java
@@ -7,23 +7,23 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.com.didi.service.RecruitServiceImpl;
 import sun.com.didi.util.EsClient;
 
-import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class EsQueryController {
         @PostMapping(value = "/ES")
         @ResponseBody
-    public void MatchAllQuery() throws IOException {
-
-        RestHighLevelClient client= EsClient.getClient();
+    public Set<Map.Entry<String, Object>> MatchAllQuery() throws IOException {
+            RestHighLevelClient client= EsClient.getClient();
+      /*   matchAllQuery()   match查询
+       RestHighLevelClient client= EsClient.getClient();
         SearchRequest request=new SearchRequest("recruit");
         // request.types(type);
 
@@ -43,7 +43,35 @@ public class EsQueryController {
         for (SearchHit hit: resp.getHits().getHits()) {
             System.out.println(hit.getSourceAsMap());
         }
-        System.out.println(resp.getHits().getHits().length);
+        System.out.println(resp.getHits().getHits().length);*/
 
+            Map<String, Object> map=null;
+            //创建对象
+            SearchRequest searchRequest = new SearchRequest("recruit");//指定索引
+            //  searchRequest.types(type);//指定类型
+
+            //        2.指定查询条件(先用searchSourceBuilder,再searchrequest指定searchSourceBuilder )
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+            searchSourceBuilder.from(0);
+            searchSourceBuilder.size(20);
+            searchSourceBuilder.query(QueryBuilders.multiMatchQuery("天津盛和集团有限公司","rec_company"));
+
+            System.out.println(searchRequest.source(searchSourceBuilder));
+
+            //执行查询
+            SearchResponse resp = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHit[] hits = resp.getHits().getHits();//从kibana里的结果可以看出来有两个hits
+            for (SearchHit searchHit : hits){
+                System.out.println(searchHit.getScore());
+                System.out.println(searchHit.getIndex());
+                map = searchHit.getSourceAsMap();
+                System.out.println(searchHit.getId());
+                for (Map.Entry<String,Object> s:map.entrySet()){
+                    System.out.println(s.getKey()+"==========="+s.getValue());
+
+                }
+                System.out.println("_----------------------");
+            }
+            return map.entrySet();
     }
 }
