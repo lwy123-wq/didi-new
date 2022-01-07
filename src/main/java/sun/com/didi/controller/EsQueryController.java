19b496074c0a46be05fe6df6268ef166b59783d3
lwy123-wq <l2158223895@qq.com>
2022-01-07 19:38:20 +0800

    bloomfilter

--- src/main/java/sun/com/didi/controller/EsQueryController.java
+++ src/main/java/sun/com/didi/controller/EsQueryController.java
@@ -8,10 +8,14 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import sun.com.didi.filter.BloomFilter;
+import sun.com.didi.service.BloomFilterService;
 import sun.com.didi.util.EsClient;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
@@ -20,6 +24,11 @@ import java.util.Map;
 
 @Controller
 public class EsQueryController {
+    @Resource
+    private BloomFilter bloomFilter;
+    @Autowired
+    private BloomFilterService bloomFilterService;
+
     @RequestMapping(value = "/search",method = RequestMethod.GET)
     public String start(){
         return "search";
@@ -27,32 +36,38 @@ public class EsQueryController {
     @PostMapping(value = "/MatchAllQuery")
     @ResponseBody
     public  List<Map<String, Object>> MatchAllQuery(@RequestBody String doc) throws IOException {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        String str = URLDecoder.decode(doc, "utf-8");
-        String stri[] = str.split("=");
-        String query = stri[1];
-        SearchResponse resp;
-        try (RestHighLevelClient client = EsClient.getClient()) {
-            //创建对象
-            SearchRequest searchRequest = new SearchRequest("recruit");//指定索引
-            //  searchRequest.types(type);//指定类型
-            //  2.指定查询条件(先用searchSourceBuilder,再searchrequest指定searchSourceBuilder )
-            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-            searchSourceBuilder.from(0);
-            searchSourceBuilder.size(5);
-            searchSourceBuilder.query(QueryBuilders.multiMatchQuery(query,"rec_company"));
 
-            System.out.println(searchRequest.source(searchSourceBuilder));
+        boolean bloom=bloomFilterService.includeByBloomFilter(bloomFilter,doc,doc);
+        if (bloom==true) {
+            List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+            String str = URLDecoder.decode(doc, "utf-8");
+            String stri[] = str.split("=");
+            String query = stri[1];
+            SearchResponse resp;
+            try (RestHighLevelClient client = EsClient.getClient()) {
+                //创建对象
+                SearchRequest searchRequest = new SearchRequest("recruit");//指定索引
+                //  searchRequest.types(type);//指定类型
+                //  2.指定查询条件(先用searchSourceBuilder,再searchrequest指定searchSourceBuilder )
+                SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+                searchSourceBuilder.from(0);
+                searchSourceBuilder.size(5);
+                searchSourceBuilder.query(QueryBuilders.multiMatchQuery(query, "rec_company"));
 
-            //执行查询
-            resp = client.search(searchRequest, RequestOptions.DEFAULT);
-        }
-        SearchHits hits = resp.getHits();//从kibana里的结果可以看出来有两个hits
-            for (SearchHit searchHit : hits){
+                System.out.println(searchRequest.source(searchSourceBuilder));
+
+                //执行查询
+                resp = client.search(searchRequest, RequestOptions.DEFAULT);
+            }
+            SearchHits hits = resp.getHits();//从kibana里的结果可以看出来有两个hits
+            for (SearchHit searchHit : hits) {
                 Map<String, Object> map = searchHit.getSourceAsMap();
                 list.add(map);
-               // System.out.println("_----------------------"+map.entrySet());
+                // System.out.println("_----------------------"+map.entrySet());
             }
             return list;
+        }else {
+            return null;
+        }
     }
 }
