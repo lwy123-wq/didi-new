dd11359812a78f78f3a42bd58e0cd1a47126b2d3
lxj <1873579697@qq.com>
2022-01-10 22:12:37 +0800

    ok

--- src/main/java/sun/com/didi/controller/HostDataController.java
+++ src/main/java/sun/com/didi/controller/HostDataController.java
@@ -11,18 +11,22 @@ import java.util.ArrayList;
 
 @Controller
 public class HostDataController {
+    private static int i=1;
     @PostMapping(value = "/LRU")
     @ResponseBody
-    public ArrayList<String> lru(@RequestBody String str) throws Exception {
-        System.out.println(str);
+    public  ArrayList<String> lru(@RequestBody String str) throws Exception {
         String doc = URLDecoder.decode(str, "utf-8");
         String stri[] = doc.split("=");
         String query = stri[1];
         LruCacheUtil cache = new LruCacheUtil(2);
         ArrayList<String> arrayList=new ArrayList<>();
-        for (int i=1;i<=3;i++) {
+
+        for ( ;i<=3;i++){
             cache.put(i, query);
+            System.out.println(i);
+            break;
         }
+
         for (int j=1;j<=3;j++){
             System.out.println(cache.get(j));
             arrayList.add((String) cache.get(j));
