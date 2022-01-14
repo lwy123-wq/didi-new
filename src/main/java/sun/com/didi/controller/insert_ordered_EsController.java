e08bcb1c2ce6ee344f127b7483cf60143af1dca1
lxj <1873579697@qq.com>
2022-01-14 23:37:24 +0800

    ok

---
+++ src/main/java/sun/com/didi/controller/insert_ordered_EsController.java
@@ -0,0 +1,53 @@
+package sun.com.didi.controller;
+
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import sun.com.didi.util.EsClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class insert_ordered_EsController {
+    @RequestMapping(value = "/insert_info",method = RequestMethod.GET)
+    public String index(){
+        return "insert_info";
+    }
+    @PostMapping(value = "/insert_ordered_Es")
+    @ResponseBody
+    //es 过滤字段
+    public List<Map<String, Object>> insert_ordered_Es(@RequestBody String Params) throws IOException {
+        List<Map<String, Object>> arrayList = new ArrayList<Map<String, Object>>();
+        SearchRequest searchRequest = new SearchRequest("recruit");//指定索引
+        RestHighLevelClient client = EsClient.getClient();
+        SearchSourceBuilder builder=new SearchSourceBuilder().query(QueryBuilders.matchAllQuery());
+        String [] exclides={};//要排除的字段
+        String [] includes={"rec_category"};//要查询的指定字段
+        builder.fetchSource(includes,exclides);
+
+        searchRequest.source(builder);
+
+        SearchResponse response=client.search(searchRequest, RequestOptions.DEFAULT);
+
+        SearchHits hits=response.getHits();
+        System.out.println(hits.getTotalHits());
+        System.out.println(response.getTook());
+
+        for (SearchHit hit: hits){
+            System.out.println(hit.getSourceAsString());
+            Map<String, Object> map =hit.getSourceAsMap();
+            arrayList.add(map);
+        }
+        return arrayList;
+    }
+}
